93600ba38b44dc07ab9fe2873f5785c762c0380f
Hyung-Suk Kim <hyung.suk.kim@icloud.com>
2021-01-11 16:39:45 -0800

    updated name

--- convert_ufo.py
+++ convert_ufo.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Mental Type Design. All Rights Reserved.
+# Copyright 2020 Mental Design. All Rights Reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
